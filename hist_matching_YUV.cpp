7d50aa79cffcaec282a26da4273bfd3a6483d17c
심정민 <37237145+jmini1234@users.noreply.github.com>
2020-04-07 00:34:15 +0900

    Update hist_matching_YUV.cpp

--- hist_matching_YUV.cpp
+++ hist_matching_YUV.cpp
@@ -15,14 +15,16 @@ int main() {
 	cvtColor(input, matching_input, CV_RGB2YUV);	// convert RGB to Grayscale
 	cvtColor(reference, matching_reference, CV_RGB2YUV);
 
-//	 split each channel(Y, U, V)
+//	 split each channel(Y, U, V) - input 
 	Mat channels[3];
 	split(matching_input, channels);
-	Mat Y = channels[0];						// U = channels[1], V = channels[2]
+	Mat Y = channels[0];						// U = channels[1], V = channels[2] - input
+
+//	 split each channel(Y, U, V) - reference
 
 	Mat channels_matching[3];
 	split(matching_reference, channels_matching);
-	Mat Y_matching = channels_matching[0];
+	Mat Y_matching = channels_matching[0];		// U = channels[1], V = channels[2] - reference
 
 //	 PDF or transfer function txt files
 //	 original 
@@ -39,24 +41,30 @@ int main() {
 	fopen_s(&f_matched_PDF_YUV, "YUV_output_PDF_matching.txt", "w+");
 	fopen_s(&f_trans_func_mat_YUV, "YUV_trans_func_mat.txt", "w+");
 
+	// Y에 대해서만 CDF를 각각 계산한다.
 	float **original_PDF = cal_PDF_RGB(matching_input);	
 	float *original_CDF = cal_CDF(Y);
 
 	float **reference_PDF = cal_PDF_RGB(matching_reference);	
 	float *reference_CDF = cal_CDF(Y_matching);	
 
-	G trans_func_mat_T[L] = { 0 };			// transfer function
-	G trans_func_mat_G[L] = { 0 };			// transfer function
-	G matching_func[L] = { 0 };			// transfer function
+	G trans_func_mat_T[L] = { 0 };			// transfer function - T
+	G trans_func_mat_G[L] = { 0 };			// transfer function - G
+	G matching_func[L] = { 0 };				// transfer function
 
+	// compute trans function using Y of each image(input, reference)
 	compute_function(trans_func_mat_T, trans_func_mat_G, matching_func, original_CDF, reference_CDF);
 
+	//	histogram matching of Y  using matching_func 
 	hist_matching(Y, Y, matching_func);
 
+	//	merging channels of input to matching_input  
 	merge(channels, 3, matching_input);
 
+	//	change matching_input from YUV to RGB
 	cvtColor(matching_input, matching_input, CV_YUV2RGB);
 
+	//	calculate PDF of matching input
 	float **matched_PDF_YUV = cal_PDF_RGB(matching_input);
 
 	for (int i = 0; i < L; i++) {
@@ -69,7 +77,7 @@ int main() {
 		//		 write output pdf  
 		fprintf(f_matched_PDF_YUV, "%d\t%f %f %f\n", i, matched_PDF_YUV[i][0], matched_PDF_YUV[i][1], matched_PDF_YUV[i][2]);
 
-	//	 write transfer functions
+		//		 write transfer functions
 		fprintf(f_trans_func_mat_YUV, "%d\t%d\n", i, matching_func[i]);
 
 
@@ -120,12 +128,13 @@ void compute_function(G *trans_func_T, G *trans_func_G, G*matching_func, float *
 		int tmp = trans_func_T[i];
 		for (int j = 0; j < L; j++) {
 			int flag = 0;
+			// 역함수를 구해야하기 때문에 G 함수에서 tmp와 맞는 j 값을 mathcing 함수에 넣어준다. 
 			if (trans_func_G[j] == tmp) {
 				matching_func[i] = j;
 				flag = 1;
 				break;
 			}
-			// 만약 matching되는 값이 없으면 nearest value 
+			// 만약 matching되는 값이 끝까지 없으면 nearest value (1개 이전의 값)을 대입한다.
 			else if (flag == 0 && j == L - 1) {
 				matching_func[i] = matching_func[i-1];
 			}
@@ -138,6 +147,7 @@ void hist_matching(Mat &input, Mat &matching, G *matching_func) {
 	 //perform the transfer function
 	for (int i = 0; i < input.rows; i++)
 		for (int j = 0; j < input.cols; j++) {
-			matching.at<G>(i,j) = matching_func[input.at<G>(i, j)]; // T(r) 즉 s 
+			// matching 함수를 통해 matching image의 pixel intensity를 대입한다.
+			matching.at<G>(i,j) = matching_func[input.at<G>(i, j)]; 
 		}
 }
